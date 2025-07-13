21e56e6ec4b906dc7a118abb3509c07e0b02a327
Yasmin Macedo <yasmin.macedo@injunior.com.br>
2025-07-13 19:42:21 -0300

    feat: implentando explosao da bomba

--- Bomba.py
+++ Bomba.py
@@ -2,7 +2,7 @@ from PPlay.sprite import*
 import time
 
 class Bomba:
-    def __init__(self, linha, coluna, tileSize, janela, tempo_antes_explodir=2):
+    def __init__(self, linha, coluna, tileSize, janela, tempo_antes_explodir=2, tempo_duracao_explosao=2):
         self.janela = janela  
         self.linha = linha
         self.coluna = coluna
@@ -13,43 +13,66 @@ class Bomba:
         self.tempo_antes_explodir = tempo_antes_explodir
         self.finalizada = False
 
-        # Usa o método da janela para carregar e escalar o sprite da bomba
         self.bomba = self.janela.carregar_sprite_escalado(
             "assets/bomb3.png", 32, 32, frames=1
         )
         self.bomba.set_position(coluna * tileSize, linha * tileSize)
 
-        self.explosao = [
-            self.janela.carregar_sprite_escalado(
-                f"assets/explosion{i}.png", 32, 32, frames=4
-            )
-            for i in range(1, 7)
-        ]
-        for spr in self.explosao:
-            spr.set_position(coluna * tileSize, linha * tileSize)
-            spr.set_total_duration(300)  
+        self.explosao = self.janela.carregar_sprite_escalado(
+            "assets/explosao_spritesheet.png", 32, 32, frames=4
+        )
+        self.explosao.set_total_duration(300)
+        self.tempo_duracao_explosao = tempo_duracao_explosao
+        self.tempo_inicio_explosao = None
 
         self.frameAtual = 0
+        self.tiles_explosao = []
+
+    def propagar_explosao(self, mapa):
+        direcoes = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        alcance = 2  # 2 tiles para cada direcao em sentido de cruz
+        explosoes = [(self.linha, self.coluna)]
+
+        for dx, dy in direcoes:
+            for i in range(1, alcance + 1):
+                nl = self.linha + dy * i
+                nc = self.coluna + dx * i
+
+                if 0 <= nl < len(mapa) and 0 <= nc < len(mapa[0]):
+                    tile = mapa[nl][nc]
+                    if tile == "B":
+                        mapa[nl][nc] = "I"  # destrói bloco
+                        explosoes.append((nl, nc))
+                        break
+                    elif tile in "WXYZ":  # parede sólida
+                        break
+                    else:
+                        explosoes.append((nl, nc))
+        self.tiles_explosao = explosoes
 
     def update(self):
         if not self.explodiu:
             if time.time() - self.tempo_criacao >= self.tempo_antes_explodir:
                 self.explodiu = True
+                self.propagar_explosao(self.janela.getMapa())
+                self.tempo_inicio_explosao = time.time()  
+                self.explosao.set_curr_frame(0)
+                self.finalizada = False
         else:
-            if self.frameAtual < len(self.explosao):
-                self.explosao[self.frameAtual].update()
-                explosao = self.explosao[self.frameAtual]
+            self.explosao.update()
 
-                if explosao.get_curr_frame() == explosao.get_final_frame():
-                    self.finalizada = True
+            if time.time() - self.tempo_inicio_explosao >= self.tempo_duracao_explosao:
+                self.finalizada = True
 
     def draw(self):
         if not self.explodiu:
             self.bomba.draw()
-        elif self.frameAtual < len(self.explosao):
-            self.explosao[self.frameAtual].draw()
+        else:
+            for linha, coluna in self.tiles_explosao:
+                self.explosao.set_position(coluna * self.tileSize, linha * self.tileSize)
+                self.explosao.draw()
         
     def terminou(self):
-        return self.explodiu and self.frameAtual >= len(self.explosao)
+        return self.explodiu and self.finalizada
     
     
